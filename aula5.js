f504374dba2f9fae6303070076c2c35f5762a8d1
Cristina Credie <c2midia@gmail.com>
2020-12-03 21:34:05 -0300

    adicionandogetProdutos

--- aula5.js
+++ aula5.js
@@ -2,14 +2,7 @@ function mostrarNome() {
     console.log("Cristina")
 }
  console.log("Cristina")
- console.log("Cristina 1")
- console.log("Cristina 2")
- console.log("Cristina 3")
- console.log("Cristina 4")
- console.log("Cristina 5")
 
- const products = ['geladeira', 'televisao' , 'radio']
-
- for (product of products) {
-     console.log(product)
+ function getProdutos () {
+     return "geladeira"
  }
